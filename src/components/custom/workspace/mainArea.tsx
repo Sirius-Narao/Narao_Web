853d552793f265a328f07f821de78e16059f7266
Sirius-Narao <dow.devotion@gmail.com>
2026-02-04 18:18:03 +0100

    feat: Add global styles, theming, and foundational UI components for the workspace layout.

--- src/components/custom/workspace/mainArea.tsx
+++ src/components/custom/workspace/mainArea.tsx
@@ -1,10 +1,75 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
+import { InputGroup, InputGroupAddon, InputGroupInput, InputGroupText } from "@/components/ui/input-group";
+import { Kbd, KbdGroup } from "@/components/ui/kbd";
 import { SidebarInset } from "@/components/ui/sidebar";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { MoveLeft, MoveRight, Search } from "lucide-react";
 
 export default function MainArea() {
     return (
         <SidebarInset className="bg-background">
+
+            {/* ------------------ Top Part -------------------- */}
+            <div className="bg-background text-foreground h-[calc(100%-94vh)] w-[calc(100%-0.5rem)] rounded-lg absolute top-2 flex items-center justify-between">
+                {/* Left Side */}
+                <div className="flex items-center gap-2">
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Button variant="ghost" className="w-10 h-10 p-0 rounded-full">
+                                <MoveLeft size={24} color="white" />
+                            </Button>
+                        </TooltipTrigger>
+                        <TooltipContent className="flex items-center gap-2">
+                            <p>Go Back</p>
+                            <KbdGroup>
+                                <Kbd className="bg-popover text-foreground">Ctrl + Z</Kbd>
+                            </KbdGroup>
+                        </TooltipContent>
+                    </Tooltip>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Button variant="ghost" className="w-10 h-10 p-0 rounded-full">
+                                <MoveRight size={24} color="white" />
+                            </Button>
+                        </TooltipTrigger>
+                        <TooltipContent className="flex items-center gap-2">
+                            <p>Go Forward</p>
+                            <KbdGroup>
+                                <Kbd className="bg-popover text-foreground">Ctrl + Alt + Z</Kbd>
+                            </KbdGroup>
+                        </TooltipContent>
+                    </Tooltip>
+                </div>
+
+                {/* Middle */}
+                <div className="">
+                    <InputGroup className="w-[40%] bg-card shadow-lg cursor-pointer px-2"
+                        onClick={() => { }}>
+                        <InputGroupAddon align="inline-end" className="cursor-pointer">
+                            <InputGroupText className="bg-transparent cursor-pointer">
+                                <KbdGroup className="">
+                                    <Kbd className="bg-popover text-muted-foreground">Ctrl + Alt + K</Kbd>
+                                </KbdGroup>
+                                <Search />
+                            </InputGroupText>
+                        </InputGroupAddon>
+                        <InputGroupInput
+                            placeholder="Look for a folder or a note..."
+                            className="bg-card cursor-pointer "
+                            onChange={(e) => { }}
+                        />
+                    </InputGroup>
+                </div>
+
+                {/* Right Side */}
+                <div>
+
+                </div>
+            </div>
+
+            {/* ------------------ Bottom Part ----------------- */}
             <div className="bg-card text-foreground h-[92vh] w-[calc(100%-0.5rem)] rounded-lg absolute bottom-2 p-4 border border-sidebar-border">
                 <div className="">
 
--- src/components/custom/workspace/sidebar.tsx
+++ src/components/custom/workspace/sidebar.tsx
@@ -2,7 +2,7 @@
 
 import { Sidebar, SidebarContent, SidebarHeader, SidebarTrigger, SidebarFooter, useSidebar } from "@/components/ui/sidebar";
 import Image from "next/image";
-import { User, Settings, X, Icon, Folder, NotebookPen, MessageCircle, MessageCirclePlus, Search, MoreHorizontal, MoreVertical, Pencil, Trash2, FolderDown, CircleOff } from "lucide-react";
+import { User, Settings, X, Icon, Folder, NotebookPen, MessageCircle, MessageCirclePlus, Search, MoreHorizontal, MoreVertical, Pencil, Trash2, FolderDown, CircleOff, Sun, Bot, Bell, Tags, Star, MoveUpLeft, ArrowUpLeft } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
@@ -22,12 +22,14 @@ import {
 import { useAreaLocation } from "@/context/areaContextLocation";
 import { InputGroup, InputGroupAddon, InputGroupInput, InputGroupText } from "@/components/ui/input-group";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuPortal, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 import ChatType from "@/types/chatType";
 import dateConvert from "@/lib/dateConvert";
 import quantifyDate from "@/lib/quantifyDate";
 import handleSearch from "@/lib/handleSearch";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Kbd, KbdGroup } from "@/components/ui/kbd";
 
 export default function SidebarArea() {
     // EXAMPLE DATA
@@ -54,7 +56,8 @@ export default function SidebarArea() {
     const { state, setOpen } = useSidebar();
     // area location
     const { areaLocation, setAreaLocation } = useAreaLocation();
-    // console.log(areaLocation);
+    // settings tab
+    const [settingsTab, setSettingsTab] = useState(0);
 
     console.table(
         CHAT_LIST_EXAMPLE.map(c => ({
@@ -88,13 +91,16 @@ export default function SidebarArea() {
                 <TooltipTrigger asChild>
                     <SidebarTrigger className="absolute top-4 right-2.5" />
                 </TooltipTrigger>
-                <TooltipContent side="bottom">
+                <TooltipContent side="bottom" className="flex items-center gap-2">
                     <p>{state === "collapsed" ? "Open" : "Close"}</p>
+                    <KbdGroup>
+                        <Kbd className="bg-popover text-foreground">Ctrl + B</Kbd>
+                    </KbdGroup>
                 </TooltipContent>
             </Tooltip>
 
             {/* --------------------------- Content --------------------------- */}
-            <SidebarContent className="bg-background mt-[9px] overflow-y-hidden">
+            <SidebarContent className="bg-background mt-[9px] overflow-hidden">
                 {/* Slider between tabs */}
                 <div className={cn(
                     "bg-card border border-sidebar-border shadow-lg relative flex items-center justify-between transition-all duration-300 ease-in-out",
@@ -139,8 +145,11 @@ export default function SidebarArea() {
                                             {index === 0 ? <Folder /> : index === 1 ? <NotebookPen /> : <MessageCirclePlus />}
                                         </Button>
                                     </TooltipTrigger>
-                                    <TooltipContent>
+                                    <TooltipContent className="flex items-center gap-2">
                                         <p>{index === 0 ? "Open Folders" : index === 1 ? "New Note" : "New Chat"}</p>
+                                        <KbdGroup>
+                                            <Kbd className="bg-popover text-foreground">Ctrl + {index === 0 ? "O" : index === 1 ? "N" : "C"}</Kbd>
+                                        </KbdGroup>
                                     </TooltipContent>
                                 </Tooltip>
                             </div>
@@ -154,6 +163,9 @@ export default function SidebarArea() {
                         onClick={() => { if (state === "collapsed") { setOpen(true) } }}>
                         <InputGroupAddon align="inline-end" className="group-data-[state=collapsed]:pr-0 group-data-[state=collapsed]:w-full group-data-[state=collapsed]:h-full group-data-[state=collapsed]:justify-center transition-all duration-200 cursor-pointer hover:bg-card/30">
                             <InputGroupText className="bg-transparent group-data-[state=collapsed]:p-0 cursor-pointer hover:bg-card/30">
+                                <KbdGroup className="group-data-[state=collapsed]:hidden">
+                                    <Kbd className="bg-popover text-muted-foreground">Ctrl + K</Kbd>
+                                </KbdGroup>
                                 <Search />
                             </InputGroupText>
                         </InputGroupAddon>
@@ -163,9 +175,9 @@ export default function SidebarArea() {
                             onChange={(e) => setFilteredChats(handleSearch(e.target.value, CHAT_LIST_EXAMPLE))}
                         />
                     </InputGroup>
-                    <ScrollArea className=" max-h-[calc(100vh-20rem)] h-fit rounded-lg border border-sidebar-border mt-2 pt-1 px-1 shadow-lg bg-card group-data-[state=collapsed]:hidden">
+                    <ScrollArea className=" max-h-[calc(100vh-20rem)] h-fit rounded-lg border border-sidebar-border mt-2 pt-1 px-1 shadow-lg bg-card/30s group-data-[state=collapsed]:hidden">
                         {filteredChats.length > 0 ? filteredChats.map((chat, index) => (
-                            <div key={index} className="flex items-center justify-between pl-4 pr-2 py-2 rounded-lg hover:bg-background/30 cursor-pointer transition-all duration-100 ease-in-out mb-1">
+                            <div key={index} className="flex items-center justify-between pl-4 pr-2 py-2 rounded-lg hover:bg-card/80 cursor-pointer transition-all duration-100 ease-in-out mb-1">
                                 <Tooltip>
                                     <TooltipTrigger asChild>
                                         <div className="flex flex-row items-center gap-2 w-full">
@@ -226,7 +238,7 @@ export default function SidebarArea() {
             </SidebarContent>
 
             {/* --------------------------- Footer --------------------------- */}
-            <SidebarFooter className="bg-background">
+            <SidebarFooter className="bg-background p-0 group-data-[state=collapsed]:pl-1 transition-all duration-200">
                 <AnimatePresence>
                     {showAnnounce && (
                         <motion.div
@@ -266,25 +278,85 @@ export default function SidebarArea() {
                         </motion.div>
                     )}
                 </AnimatePresence>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <Button variant="ghost" className={cn(
-                            "flex items-center justify-center z-30 gap-2 w-full h-10 border border-sidebar-border rounded-full px-2 bg-card shadow-lg overflow-hidden",
-                            "group-data-[state=collapsed]:w-10 group-data-[state=collapsed]:h-10 group-data-[state=collapsed]:items-center group-data-[state=collapsed]:justify-center group-data-[state=collapsed]:gap-0"
-                        )}>
-                            <User size={24} className="w-24 h-24" />
-                            <span className="text-lg font-bold transition-all duration-200 group-data-[state=collapsed]:hidden">
-                                {USERNAME_EXAMPLE}
-                            </span>
-                            <div className="flex items-center justify-end w-full">
-                                <Settings size={24} className="group-data-[state=collapsed]:hidden w-full transition-all duration-200" />
+                <Dialog>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <DialogTrigger asChild>
+                                <Button variant="ghost" className={cn(
+                                    "flex items-center justify-center z-30 gap-2 w-full h-10 border border-sidebar-border rounded-full px-2 bg-card shadow-lg overflow-hidden",
+                                    "group-data-[state=collapsed]:w-10 group-data-[state=collapsed]:h-10 group-data-[state=collapsed]:items-center group-data-[state=collapsed]:justify-center group-data-[state=collapsed]:gap-0"
+                                )}>
+                                    <User size={24} className="w-24 h-24" />
+                                    <span className="text-lg font-bold transition-all duration-200 group-data-[state=collapsed]:hidden">
+                                        {USERNAME_EXAMPLE}
+                                    </span>
+                                    <div className="flex items-center justify-end w-full">
+                                        <Settings size={24} className="group-data-[state=collapsed]:hidden w-full transition-all duration-200" />
+                                    </div>
+                                </Button>
+                            </DialogTrigger>
+                        </TooltipTrigger>
+                        <TooltipContent className="flex items-center gap-2">
+                            <p>Settings</p>
+                            <KbdGroup>
+                                <Kbd className="bg-popover text-foreground">Ctrl + ,</Kbd>
+                            </KbdGroup>
+                        </TooltipContent>
+                    </Tooltip>
+                    <DialogContent className="max-w-5xl grid grid-cols-5 " showCloseButton={false}>
+                        <div className="col-span-1 h-full flex flex-col gap-2 relative">
+                            <div className={cn("flex items-center justify-center w-full gap-2 p-4 py-2 rounded-lg border border-sidebar-border hover:bg-card/50 cursor-pointer transition-all duration-200 text-left justify-start ", settingsTab === 0 && "bg-card hover:bg-card")}
+                                onClick={() => { setSettingsTab(0) }}>
+                                <Sun size={20} />
+                                <p className="text-sm font-medium">Preferences</p>
                             </div>
-                        </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                        <p>Settings</p>
-                    </TooltipContent>
-                </Tooltip>
+                            <div className={cn("flex items-center justify-center w-full gap-2 p-4 py-2 rounded-lg border border-sidebar-border hover:bg-card/50 cursor-pointer transition-all duration-200 text-left justify-start ", settingsTab === 1 && "bg-card hover:bg-card")}
+                                onClick={() => { setSettingsTab(1) }}>
+                                <User size={20} />
+                                <p className="text-sm font-medium">Account</p>
+                            </div>
+                            <div className={cn("flex items-center justify-center w-full gap-2 p-4 py-2 rounded-lg border border-sidebar-border hover:bg-card/50 cursor-pointer transition-all duration-200 text-left justify-start ", settingsTab === 2 && "bg-card hover:bg-card")}
+                                onClick={() => { setSettingsTab(2) }}>
+                                <Bot size={20} />
+                                <p className="text-sm font-medium">AI Settings</p>
+                            </div>
+                            <div className={cn("flex items-center justify-center w-full gap-2 p-4 py-2 rounded-lg border border-sidebar-border hover:bg-card/50 cursor-pointer transition-all duration-200 text-left justify-start ", settingsTab === 3 && "bg-card hover:bg-card")}
+                                onClick={() => { setSettingsTab(3) }}>
+                                <Tags size={20} />
+                                <p className="text-sm font-medium">Categories</p>
+                            </div>
+                            <div className={cn("flex items-center justify-center w-full gap-2 p-4 py-2 rounded-lg border bg-card border-sidebar-border hover:bg-card/50 cursor-pointer transition-all duration-200 text-left justify-start absolute bottom-0 left-0 right-0 ")}
+                                // We want it to link to the about page
+                                onClick={() => { }}>
+                                <ArrowUpLeft size={20} />
+                                <p className="text-sm font-medium">About</p>
+                            </div>
+                            {/* <div className={cn("flex items-center justify-center w-full gap-2 p-4 py-2 rounded-lg border bg-primary border-sidebar-border hover:bg-primary/80 cursor-pointer transition-all duration-200 text-left justify-start text-primary-foreground absolute bottom-0 left-0 right-0 ")}>
+                                <Star size={20} />
+                                <p className="text-sm font-medium">Try Narao Pro!</p>
+                            </div> */}
+                        </div>
+                        <div className="col-span-4 flex flex-col h-full relative px-4">
+                            <DialogHeader>
+                                <DialogTitle>{settingsTab === 0 ? "Preferences" : settingsTab === 1 ? "Account" : settingsTab === 2 ? "AI Settings" : "Categories"}</DialogTitle>
+                                <DialogDescription>
+                                    {settingsTab === 0 ? "Manage your workspace preferences." : settingsTab === 1 ? "Manage your workspace account." : settingsTab === 2 ? "Manage your workspace AI settings." : "Manage your workspace categories."}
+                                </DialogDescription>
+                            </DialogHeader>
+                            <p className="text-center text-muted-foreground h-52 flex items-center justify-center">SOON TO COME</p>
+
+                            {/* For spacing */}
+                            <div className="h-24"></div>
+
+                            <DialogFooter className="flex items-center justify-end gap-2 p-2 absolute bottom-0 left-1/2 -translate-x-1/2 bg-card/50 backdrop-blur-sm w-fit rounded-full border">
+                                <DialogClose asChild>
+                                    <Button variant="ghost">Close</Button>
+                                </DialogClose>
+                                <Button variant="default">Save Changes</Button>
+                            </DialogFooter>
+                        </div>
+                    </DialogContent>
+                </Dialog>
             </SidebarFooter>
         </Sidebar>
     );
